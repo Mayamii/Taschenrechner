f0f447556f6e5adad6216765014fd89713f153fc
Mayamii <51121925+Mayamii@users.noreply.github.com>
2021-05-06 08:55:18 +0200

    calc

---
+++ script.js
@@ -0,0 +1,93 @@
+let bufferedNumber =""
+let savedOperator =""
+let savedNumber =""
+let calcBoolean =false
+
+//resets all variables
+function reset()
+{
+    savedOperator =""
+    savedNumber =""
+    bufferedNumber=""
+    calcBoolean = false
+}
+//comes from Buttons, decides which functions to run depending on inputbutton
+function input(x){
+    switch(x){
+        case "*": 
+        case "/":
+        case "+":
+        case "-":
+        saveOperator(x)
+            break;
+        case "equals":console.log(x)
+            break;
+        case "c": reset()
+        break;
+        case "back":deleteCharacter()
+        break;
+        default: typeNumbers(x)
+            break;
+    }
+}
+
+//only exists to replace bufferedNumber with result, sets calcBoolean true
+function calculate()
+{
+    if(savedNumber!=""&& savedOperator!=""){
+        calcBoolean = true
+        var1 = parseInt(bufferedNumber)
+        var2= parseInt(savedNumber)
+    switch(savedOperator)
+    {
+        
+        case "*": var3= var1*var2
+        console.log(var3)
+        bufferedNumber =  toString(var3)
+        break;
+        case "/":bufferedNumber = toString(var1/var2)  
+        break;
+        case "+":bufferedNumber = toString(var1+var2)
+        break;
+        case "-": bufferedNumber = toString(var1-var2) 
+        break;
+        
+        
+    }
+    
+}
+}
+
+// changes bufferedNumber (adds to it), if calculated before it resets all variables
+function typeNumbers(x){
+    if (calcBoolean){
+       reset()
+    }
+    bufferedNumber = bufferedNumber+x
+console.log(bufferedNumber)
+}
+
+//removes last character of bufferedNumber
+function deleteCharacter(){
+    if(bufferedNumber!=""){
+    bufferedNumber= bufferedNumber.substring(0, bufferedNumber.length-1)}
+    console.log(bufferedNumber)
+}
+
+//when you type an operator it triggers this function. it triggers calculate if savednumber and savedoperator are already existing, then overrides savedoperator
+function saveOperator(x)
+{
+calculate()
+savedOperator=x
+savedNumber=bufferedNumber
+console.log(savedOperator)
+}
+
+//gets triggered after pressing equals, calculates and resets savedOperator and saved Number
+function equals()
+{
+    calculate()
+    savedOperator=""
+    savedNumber=""
+    console.log(bufferedNumber)
+}
